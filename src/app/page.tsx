7708b681a3a9b56f69a08bd7ffc1adabaf2fe1a2
Terry Cheng <cyr062599@gmail.com>
2024-05-20 00:13:20 +0930

    refactor: Update  layout of ModeToggle component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,27 +5,31 @@ import { Calculator } from "@/components/calculator";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 font-bold text-lg">
-          Marvel Snap Bundle Calculator
-        </p>
+    <>
+      <div className="absolute right-6 top-6 z-50 lg:right-10 lg:top-10">
         <ModeToggle />
       </div>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+          <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 font-bold text-lg">
+            Marvel Snap Bundle Calculator
+          </p>
+        </div>
 
-      <div className="mb-24 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <Calculator />
-      </div>
+        <div className="mb-24 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+          <Calculator />
+        </div>
 
-      <div className="fixed bottom-4 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none gap-3">
-        <p>
-          By {"  "}
-          <Link href="https://github.com/lemonteaau">lemontea</Link>
-        </p>
-        <Link href="https://github.com/lemonteaau">
-          <FaGithub size={24} />
-        </Link>
-      </div>
-    </main>
+        <div className="fixed bottom-4 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none gap-3">
+          <p>
+            By {"  "}
+            <Link href="https://github.com/lemonteaau">lemontea</Link>
+          </p>
+          <Link href="https://github.com/lemonteaau">
+            <FaGithub size={24} />
+          </Link>
+        </div>
+      </main>
+    </>
   );
 }
