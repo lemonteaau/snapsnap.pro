64fdb23be7b1f9612912853085a44bce95e65e9c
Terry Cheng <cyr062599@gmail.com>
2024-05-21 19:34:24 +0930

    refactor: Refactor page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,31 +5,41 @@ import { Calculator } from "@/components/calculator";
 
 export default function Home() {
   return (
-    <>
-      <div className="fixed right-6 top-6 z-50 lg:right-10 lg:top-10">
-        <ModeToggle />
-      </div>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-          <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 font-bold text-lg">
-            Snap Bundle Calculator
-          </p>
-        </div>
-
-        <div className="mb-24 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-          <Calculator />
-        </div>
-
-        <div className="fixed bottom-4 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none gap-3">
-          <p>
-            By {"  "}
-            <Link href="https://github.com/lemonteaau">lemontea</Link>
-          </p>
-          <Link href="https://github.com/lemonteaau">
-            <FaGithub size={24} />
-          </Link>
-        </div>
+    <div className="h-screen">
+      <main className="flex flex-col items-center gap-6 p-20">
+        <Header />
+        <Calculator />
       </main>
-    </>
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header className="z-10 w-full max-w-5xl font-mono text-sm">
+      <div className="fixed left-0 top-0 flex w-full items-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <p className="flex-grow text-center text-xl font-bold">
+          Snap Bundle Calculator
+        </p>
+        <div className="ml-auto mr-4">
+          <ModeToggle />
+        </div>
+      </div>
+    </header>
+  );
+}
+
+function Footer() {
+  return (
+    <footer className="fixed bottom-4 left-0 flex h-10 w-full items-end justify-center gap-3 bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
+      <p>
+        By {"  "}
+        <Link href="https://github.com/lemonteaau">lemontea</Link>
+      </p>
+      <Link href="https://github.com/lemonteaau">
+        <FaGithub size={24} />
+      </Link>
+    </footer>
   );
 }
