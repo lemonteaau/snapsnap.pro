d3ef52f1bbcadb10d5fc933736865535bdfc016d
Terry Cheng <cyr062599@gmail.com>
2024-05-22 21:22:20 +0930

    chore: commented out i18n configuration in next.config.mjs for temporary

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n: {
-    locales: ["en", "zh-CN"],
-    defaultLocale: "en",
-  },
+  // i18n: {
+  //   locales: ["en", "zh-CN"],
+  //   defaultLocale: "en",
+  // },
 };
 
 export default nextConfig;
