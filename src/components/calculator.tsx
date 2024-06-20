3d8e61ecdba0848ac9fa2ed7154113262a118f41
Terry Cheng <cyr062599@gmail.com>
2024-06-20 17:44:44 +0930

    feat: Add support for AUD and CNY

--- src/components/calculator.tsx
+++ src/components/calculator.tsx
@@ -5,7 +5,9 @@ import Image from "next/image";
 import { Check, CircleDollarSign } from "lucide-react";
 import { LuPlus, LuMinus } from "react-icons/lu";
 import { GrPowerReset } from "react-icons/gr";
-
+import aus from "/icon/aus.svg";
+import chn from "/icon/chn.svg";
+import usa from "/icon/usa.svg";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -15,6 +17,13 @@ import {
   CardHeader,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { useTranslation } from "react-i18next";
 import i18n from "@/lib/i18n";
 
@@ -39,8 +48,17 @@ export function Calculator() {
   const [price, setPrice] = useState(0);
   const [value, setValue] = useState(0);
   const [suggestion, setSuggestion] = useState("");
+  const [currency, setCurrency] = useState("USD");
+
+  //Exchange rate for currency.
+  const exchangeRates: { [key: string]: number } = {
+    CNY: 6.8,
+    AUD: 1.5,
+    USD: 1,
+  };
 
   const calculateValue = () => {
+    const priceInUSD = price / exchangeRates[currency];
     // Calculate the value of the items based on gold.
     const gvarientValue = gvarient * 1200;
     const pvarientValue = pvarient * 700;
@@ -48,7 +66,7 @@ export function Calculator() {
     const totalGold = gold + (credit / 5) * 4 + (token / 3) * 4;
     const totalValueGold =
       gvarientValue + pvarientValue + cacheKeyValue + totalGold;
-    const totalPriceGold = price * 100;
+    const totalPriceGold = priceInUSD * 100;
 
     if (totalPriceGold === 0) {
       setValue(0);
@@ -84,6 +102,12 @@ export function Calculator() {
     setSuggestion("");
   };
 
+  const currencyMap: { [key: string]: string } = {
+    AUD: t("AUD"),
+    CNY: t("CNY"),
+    USD: t("USD"),
+  };
+
   return (
     <div className="mb-8 max-w-[380px] text-center lg:mb-0 lg:w-full lg:min-w-[900px] lg:px-32">
       <Card>
@@ -91,7 +115,7 @@ export function Calculator() {
           <CardDescription>{t("fill_form")}</CardDescription>
         </CardHeader>
         <CardContent>
-          <div className="flex flex-col gap-5">
+          <div className="flex flex-col gap-7">
             <div className="mb-3 grid grid-cols-2 gap-6 lg:grid-cols-3">
               {[
                 {
@@ -179,10 +203,55 @@ export function Calculator() {
               <CircleDollarSign className="h-8 w-8" />
               <Input
                 type="number"
-                placeholder={t("price_in_usd")}
+                placeholder={t("price")}
                 value={price === 0 ? "" : price}
+                className="max-w-[120px]"
                 onChange={(e) => setPrice(Number(e.target.value))}
               />
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button variant="outline" className="max-w-[60px]">
+                    {currencyMap[currency]}
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-8">
+                  <DropdownMenuRadioGroup
+                    value={currency}
+                    onValueChange={setCurrency}
+                  >
+                    <DropdownMenuRadioItem value="AUD">
+                      <Image
+                        height={18}
+                        width={18}
+                        src={"/icon/aus.svg"}
+                        alt="Flag of Australia"
+                        className="mr-2"
+                      />
+                      {t("AUD")}
+                    </DropdownMenuRadioItem>
+                    <DropdownMenuRadioItem value="CNY">
+                      <Image
+                        height={18}
+                        width={18}
+                        src={"/icon/chn.svg"}
+                        alt="Flag of China"
+                        className="mr-2"
+                      />
+                      {t("CNY")}
+                    </DropdownMenuRadioItem>
+                    <DropdownMenuRadioItem value="USD">
+                      <Image
+                        height={18}
+                        width={18}
+                        src={"/icon/usa.svg"}
+                        alt="Flag of USA"
+                        className="mr-2"
+                      />
+                      {t("USD")}
+                    </DropdownMenuRadioItem>
+                  </DropdownMenuRadioGroup>
+                </DropdownMenuContent>
+              </DropdownMenu>
             </div>
           </div>
           {value !== null && (
