1e868150a92ae412efe3512feff5bf2e99897dbf
Terry Cheng <cyr062599@gmail.com>
2024-06-27 20:01:52 +0930

    chore: Update text by replacing "Dark mode setting" with "Theme setting"

--- src/components/settings.tsx
+++ src/components/settings.tsx
@@ -42,7 +42,7 @@ export function Settings({ t }: { t: (key: string) => string }) {
               <DropdownMenuSubTrigger>
                 <LuSun className="mr-2 h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
                 <LuMoonStar className="absolute mr-2 h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                <span>{t("dmode_setting")}</span>
+                <span>{t("theme_setting")}</span>
               </DropdownMenuSubTrigger>
               <DropdownMenuPortal>
                 <DropdownMenuSubContent>
